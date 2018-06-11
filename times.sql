4f5e20f39d0c00c5796bc289819625ab9c4abdc9
ASO1601020 <32320677+ASO1601020@users.noreply.github.com>
2018-06-11 10:27:52 +0900

    堀口

---
+++ times.sql
@@ -0,0 +1,85 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2018 年 6 朁E11 日 03:26
+-- サーバのバージョン： 10.1.21-MariaDB
+-- PHP Version: 7.0.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mydata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `times`
+--
+
+CREATE TABLE `times` (
+  `time_id` int(11) NOT NULL,
+  `time` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `times`
+--
+
+INSERT INTO `times` (`time_id`, `time`) VALUES
+(1, '0:00'),
+(2, '1:00'),
+(3, '2:00'),
+(4, '3:00'),
+(5, '4:00'),
+(6, '5:00'),
+(7, '6:00'),
+(8, '7:00'),
+(9, '8:00'),
+(10, '9:00'),
+(11, '10:00'),
+(12, '11:00'),
+(13, '12:00'),
+(14, '13:00'),
+(15, '14:00'),
+(16, '15:00'),
+(17, '16:00'),
+(18, '17:00'),
+(19, '18:00'),
+(20, '19:00'),
+(21, '20:00'),
+(22, '21:00'),
+(23, '22:00'),
+(24, '23:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `times`
+--
+ALTER TABLE `times`
+  ADD PRIMARY KEY (`time_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `times`
+--
+ALTER TABLE `times`
+  MODIFY `time_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
